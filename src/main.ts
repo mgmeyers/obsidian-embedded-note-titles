f2248f1cc71545c7bfa0f70219c790ea047b8482
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-12-12 23:36:00 -0800

    Allow using metadata field for title

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import { EditorState, StateEffect, StateField, Transaction } from "@codemirror/state";
+import {
+  EditorState,
+  StateEffect,
+  StateField,
+  Transaction,
+} from "@codemirror/state";
 import {
   Decoration,
   DecorationSet,
   EditorView,
   WidgetType,
 } from "@codemirror/view";
-import { editorViewField, MarkdownView, Plugin } from "obsidian";
+import {
+  App,
+  editorViewField,
+  MarkdownView,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
+} from "obsidian";
 import {
   LegacyCodemirrorHeadingsManager,
   PreviewHeadingsManager,
@@ -46,34 +59,55 @@ class HeaderWidget extends WidgetType {
 }
 
 export default class EmbeddedNoteTitlesPlugin extends Plugin {
+  settings: Settings;
+
   legacyCodemirrorHeadingsManager: LegacyCodemirrorHeadingsManager;
   previewHeadingsManager: PreviewHeadingsManager;
 
-  onload() {
-    this.app.workspace.trigger("parse-style-settings");
-    document.body.classList.add("embedded-note-titles");
+  async onload() {
+    await this.loadSettings();
 
+    this.addSettingTab(new EmbeddedNoteTitlesSettings(this.app, this));
+
+    this.app.workspace.trigger("parse-style-settings");
     this.previewHeadingsManager = new PreviewHeadingsManager();
+
+    document.body.classList.add("embedded-note-titles");
 
     if ((this.app.vault as any).getConfig("legacyEditor")) {
       this.legacyCodemirrorHeadingsManager =
         new LegacyCodemirrorHeadingsManager();
     } else {
       const updateTitle = StateEffect.define<boolean>();
+      const getTitleForView = (view: MarkdownView) => {
+        const frontmatterKey = this.settings.titleMetadataField;
+
+        let title = view.file?.basename;
+
+        if (frontmatterKey && view.file) {
+          const cache = this.app.metadataCache.getFileCache(view.file);
+
+          if (cache.frontmatter[frontmatterKey]) {
+            title = cache.frontmatter[frontmatterKey];
+          }
+        }
+
+        return title || "";
+      };
 
       this.registerEditorExtension([
         StateField.define<DecorationSet>({
-          create(state: EditorState) {
+          create: (state: EditorState) => {
             const view = state.field(editorViewField);
-    
+
             return Decoration.set(
               Decoration.replace({
                 block: true,
-                widget: new HeaderWidget(view.file?.basename || ''),
+                widget: new HeaderWidget(getTitleForView(view)),
               }).range(0, 0)
             );
           },
-          update(effects: DecorationSet, tr: Transaction) {
+          update: (effects: DecorationSet, tr: Transaction) => {
             for (let e of tr.effects) {
               if (e.is(updateTitle)) {
                 const view = tr.state.field(editorViewField);
@@ -81,7 +115,7 @@ export default class EmbeddedNoteTitlesPlugin extends Plugin {
                 return Decoration.set(
                   Decoration.replace({
                     block: true,
-                    widget: new HeaderWidget(view.file?.basename || ''),
+                    widget: new HeaderWidget(getTitleForView(view)),
                   }).range(0, 0)
                 );
               }
@@ -90,22 +124,34 @@ export default class EmbeddedNoteTitlesPlugin extends Plugin {
             return effects;
           },
           provide: (f) => EditorView.decorations.from(f),
-        })
+        }),
       ]);
 
-      this.registerEvent(this.app.vault.on("rename", file => {
-        const markdownLeaves = this.app.workspace.getLeavesOfType('markdown');
+      const notifyFileChange = (file: TFile) => {
+        const markdownLeaves = this.app.workspace.getLeavesOfType("markdown");
 
-        markdownLeaves.forEach(leaf => {
-          const view = (leaf.view as MarkdownView);
+        markdownLeaves.forEach((leaf) => {
+          const view = leaf.view as MarkdownView;
 
           if (view.file === file) {
             ((view.editor as any).cm as EditorView).dispatch({
-              effects: updateTitle.of(true)
-            })
+              effects: updateTitle.of(true),
+            });
+          }
+        });
+      };
+
+      this.registerEvent(this.app.vault.on("rename", notifyFileChange));
+
+      this.registerEvent(
+        this.app.metadataCache.on("changed", (file) => {
+          const frontmatterKey = this.settings.titleMetadataField;
+
+          if (frontmatterKey) {
+            notifyFileChange(file);
           }
         })
-      }))
+      );
     }
 
     this.registerEvent(
@@ -143,5 +189,46 @@ export default class EmbeddedNoteTitlesPlugin extends Plugin {
 
     this.legacyCodemirrorHeadingsManager?.cleanup();
     this.previewHeadingsManager.cleanup();
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
+
+interface Settings {
+  titleMetadataField?: string;
+}
+
+class EmbeddedNoteTitlesSettings extends PluginSettingTab {
+  plugin: EmbeddedNoteTitlesPlugin;
+
+  constructor(app: App, plugin: EmbeddedNoteTitlesPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName("Curly Quotes")
+      .setDesc(
+        "Double and single quotes will be converted to curly quotes (“” & ‘’)"
+      )
+      .addText((text) => {
+        text
+          .setValue(this.plugin.settings.titleMetadataField || "")
+          .onChange(async (value) => {
+            this.plugin.settings.titleMetadataField = value;
+            await this.plugin.saveSettings();
+          });
+      });
   }
 }
